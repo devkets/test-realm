18663df53c8dd3b1b90456efb2066587b2d62398
Andrew Lewis <ajlewis1851@gmail.com>
2024-07-30 23:19:37 -0500

    Adding array sort function

--- math.js
+++ math.js
@@ -1,5 +1,27 @@
-function addTwoNums(x, y) {
+const arr = [4,3,5,7,1,9,4,2,8,10,11,1,2,6,42,9,32];
+
+// Basic addition that returns a sum
+function sumOfTwoNums(x, y) {
     return x + y;
 }
 
-console.log(addTwoNums(4, 5));
+function printArray(arr) {
+    for(i in arr) {
+        console.log(arr[i]);
+    }
+}
+
+function sortArray(arr) {
+    for(i = 0; i < arr.length - 1; i++) {
+        for(j = 0; j < arr.length - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                let x = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = x;
+            }
+        }
+    }
+    return arr;
+}
+
+printArray(sortArray(arr));
