16a0a46b724426901dcd7aaaa0bcd54016424111
Andrew Lewis <ajlewis1851@gmail.com>
2024-07-31 19:02:39 -0500

    Fixed mergeSort to utilize double forLoop

--- math.js
+++ math.js
@@ -1,4 +1,6 @@
 const arr = [4,3,5,7,1,9,4,2,8,10,11,1,2,6,42,9,32];
+const arr2 = [4,3,5,7,1,9,4,2,8,10,11,1,2,6,42,9,32];
+
 
 // Basic addition that returns a sum
 function sumOfTwoNums(x, y) {
@@ -11,17 +13,18 @@ function printArray(arr) {
     }
 }
 
-function sortArray(arr) {
-    for(i = 0; i < arr.length - 1; i++) {
-        for(j = 0; j < arr.length - 1; j++) {
-            if(arr[j] > arr[j+1]) {
-                let x = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = x;
+function mergeSort(arr) {
+    for(i = 0; i < arr.length; i++) {
+        for(j = 1; j < arr.length; j++) {
+            if(arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
-    return arr;
 }
 
-printArray(sortArray(arr));
+console.time("Merge Sort")
+printArray(mergeSort(arr2));
+console.timeEnd("Merge Sort")
